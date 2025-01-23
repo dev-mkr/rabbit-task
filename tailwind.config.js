e4146d86085b7b0edb7943308a1bde78d5d8652d
dev-mkr <webdev.mu@gmail.com>
2025-01-23 20:12:20 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1.3rem",
+          lg: "4rem",
+        },
+        screens: {
+          lg: "1300px",
+        },
+      },
+      colors: {
+        main: "rgb(var(--color-main) / <alpha-value>)",
+      },
+      textColor: {
+        primary: "rgb(var(--color-primary) / <alpha-value>)",
+      },
+    },
+  },
+  plugins: [],
+}
+
