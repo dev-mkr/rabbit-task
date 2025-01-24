3e43146fe691314be786a54bc4dca2fbaecc80b0
dev-mkr <webdev.mu@gmail.com>
2025-01-24 03:38:32 +0200

    created: toggle theme button tests

--- src/components/shared/ToggleThemeButton/ToggleThemeButton.tsx
+++ src/components/shared/ToggleThemeButton/ToggleThemeButton.tsx
@@ -14,11 +14,18 @@ export const ToggleThemeButton = () => {
       onClick={handelToggleTheme}
       className="w-fit rounded-lg border border-gray-300 px-3 py-2.5 transition-colors hover:bg-gray-100 dark:border-gray-600 dark:hover:bg-gray-700"
       aria-label="Toggle theme mode"
+      data-testid="theme-button"
     >
       {selectedTheme === "dark" ? (
-        <Sun className="h-5 w-5 text-gray-900 dark:text-white" />
+        <Sun
+          className="h-5 w-5 text-gray-900 dark:text-white"
+          data-testid="sun-icon"
+        />
       ) : (
-        <Moon className="h-5 w-5 text-gray-900 dark:text-white" />
+        <Moon
+          className="h-5 w-5 text-gray-900 dark:text-white"
+          data-testid="moon-icon"
+        />
       )}
     </button>
   );
