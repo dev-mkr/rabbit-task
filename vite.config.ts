7d27b4312ce685d0b8b07e00520780d5f71a7ea1
dev-mkr <webdev.mu@gmail.com>
2025-01-24 03:12:03 +0200

    feat: add unit testing by vitest and react-testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -9,5 +9,10 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/tests/setup.ts",
   },
 });
